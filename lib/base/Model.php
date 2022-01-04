ed28a6f07d38d01df1ed5a46e861bb5614c8d396
Alex Ortigosa <alexortigosa179@gmail.com>
2022-01-04 11:20:38 +0100

    Model.php: Added Write JSON Method, initialized constructor and added a dbChecker function

--- lib/base/Model.php
+++ lib/base/Model.php
@@ -16,7 +16,11 @@ class Model {
     protected $dbDir = ROOT_PATH . '/db/';
 
     public function __construct(){
+        $this->parseJSON('todos');
+        $this->_todos = $this->fetchTodos();
 
+        $this->parseJSON('users');
+        $this->_users = $this->fetchUsers();
     }
 
     /**
@@ -37,17 +41,28 @@ class Model {
     }
 
     /**
+     * Writes the given array to the respective JSON database
+     * @param array $data the array data to append to the JSON database
+     * @param string $db the database being written. Only users & todos are valid values
+     */
+    public function writeJSON(array $data, string $db){
+
+        $db = $this->dbChecker($db);
+
+        array_push($this->$db, $data);
+        $rawData = json_encode($this->$db, JSON_PRETTY_PRINT);
+
+        file_put_contents($this->dbDir . substr($db, 1) . '.json', $rawData);
+    }
+
+    /**
      * Finds and return the match of the given ID
      * @param int $id the id of the todo or user
-     * @param string $db the db to search for. Only users & todos are valid values
+     * @param string $db the database to search for. Only users & todos are valid values
      */
     public function findOneById(int $id, string $db){
 
-        if($db !== 'users' && $db !== 'todos'){
-            return [];
-        } else {
-            $db = '_' . $db; 
-        }
+        $db = $this->dbChecker($db);
 
         $result = [];
 
@@ -59,6 +74,16 @@ class Model {
 
         return $result;
     }
+
+    /**
+     * Checks if DB is the correct type
+     * @param string $db the database
+     * @return exception|string returns an exception if DB wasn't users or todos, else append _ to the DB name and returns it 
+     */
+    protected function dbChecker(string $db){
+        return ($db !== 'users' && $db !== 'todos') ? throw new Exception('Not a valid Database!') : $db = '_' . $db; 
+    }
+    
 
     public function fetchUsers(){
         $this->_users = $this->_jsonData;
