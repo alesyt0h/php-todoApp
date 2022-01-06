0f9387f8064426a26519282fe30e46fa72804041
Alex Ortigosa <alexortigosa179@gmail.com>
2022-01-06 10:06:05 +0100

    User warned with a message when creating a todo without account

--- app/controllers/TodoController.php
+++ app/controllers/TodoController.php
@@ -17,13 +17,23 @@ class TodoController extends ApplicationController {
     public function newAction(){
         
         if(isset($_POST['newTodo'])){
+
             $result = $this->todoDB->createTodo($_POST['newTodo']);
 
             if($result){
+
+                if(!isset($_SESSION['loggedUser'])){
+                    $_SESSION['todoMsg'] = "You created a todo, but you don't have an account! 
+                    TODO's created without account are deleted in 24h. 
+                    <a href=" . WEB_ROOT . "/auth/register" . ">Register now to keep your TODO for ever!</a>";
+                }
+                
                 header('Location: ' . WEB_ROOT . substr($_SERVER['REQUEST_URI'], strlen(WEB_ROOT)));
+                die();
             } else {
                 $this->view->todoError = 'Error creating the todo, please try again';
             }
+
         }
     }
 
