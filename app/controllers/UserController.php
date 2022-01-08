c7d116cf712359795e1be406ab3632fbf14b5de0
Alex Ortigosa <alexortigosa179@gmail.com>
2022-01-08 07:45:18 +0100

    Users have now a dashboard to change some of their data

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -4,6 +4,13 @@ class UserController extends ApplicationController{
 
     public function profileAction(){
         
+        if(!isset($_SESSION['loggedUser'])){
+            header('Location: ' . WEB_ROOT);
+            die();
+        }
+
+        $this->view->user = $_SESSION['loggedUser'];
+
     }
 
 }
