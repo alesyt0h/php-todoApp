f6f549436aab9587d78a88ce8aa26c49076372be
Alex Ortigosa <alexortigosa179@gmail.com>
2022-01-18 18:32:37 +0100

    Usage reminder in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,3 +5,8 @@ module.exports = {
   },
   plugins: [],
 }
+
+// * Usage for production
+// curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-windows-x64.exe
+// mv tailwindcss-windows-x64.exe tailwindcss.exe
+// tailwindcss -i .\tailwind-input.css -o ./web/css/tailwind.css --minify
